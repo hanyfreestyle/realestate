9573fd7d03822819ee71c7e92f7474c180569c21
Hany Darwish <hany.freestyle4u@gmail.com>
2023-10-29 13:03:21 +0200

    update 2023-10-29

--- resources/views/components/home-page/featured-projects-inc.blade.php
+++ resources/views/components/home-page/featured-projects-inc.blade.php
@@ -1,14 +1,15 @@
 <div class="row g-4">
     <div class="col-md-6 col-xl-4">
-        <x-blog.related-projects-card :project="$projects[0]" cardstyle="Home_featured_projects_card" />
+        <x-blog.related-projects-card :project="$projects[0]" cardstyle="Home_featured_projects_card"  />
     </div>
+
     <div class="col-xl-4 order-md-3 order-xl-2">
         <div class="row g-4">
             <div class="col-md-6 col-xl-12">
-                <x-blog.related-projects-card :project="$projects[1]" />
+                <x-blog.related-projects-card :project="$projects[1]" cardstyle="" />
             </div>
             <div class="col-md-6 col-xl-12">
-                <x-blog.related-projects-card :project="$projects[2]" />
+                <x-blog.related-projects-card :project="$projects[2]" cardstyle="" />
             </div>
         </div>
     </div>
